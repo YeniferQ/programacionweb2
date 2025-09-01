5f752020908668723b7aae7855b3fec83b43259d
YeniferQ <yenifersorj@gmail.com>
2025-08-31 21:33:38 -0600

    corregido

--- Operaciones_en_ JS/app6.js
+++ Operaciones_en_ JS/app6.js
@@ -3,19 +3,14 @@ let variable2 = parseInt(prompt("Ingresar Numero 2: "));
 
 
 console.log(typeof variable1);
-
-let tmp = parseInt(variable2);
-console.log(typeof tmp);
-
-let Suma = (variable1 + variable2);
-console.log(Suma);
+console.log(typeof variable2);
 
 
-let Resta = (variable1 - variable2);
-console.log(Resta);
-
-let Multiplicacion = (variable1 * variable2);
-console.log(Multiplicacion);
-
-let Division = (variable1 / variable2);
-console.log(Division);
+let suma = (variable1 + variable2);
+console.log(suma);
+let resta = (variable1 - variable2);
+console.log(resta);
+let multiplicacion = (variable1 * variable2);
+console.log(multiplicacion);
+let division = (variable1 / variable2);
+console.log(division);
